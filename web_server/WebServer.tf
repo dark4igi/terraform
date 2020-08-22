bd7fbf9f55916b0524e56ee78a9032b04c32868a
dark4igi <dakr4igi@gmail.com>
2020-08-23 01:16:26 +0300

    up serv type

--- web_server/WebServer.tf
+++ web_server/WebServer.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "My_Web_Server" {
   ami                    = "ami-02354e95b39ca8dec"
-  instance_type          = "t2.small"
+  instance_type          = "t2.medium"
   key_name               = "aws-key"
   vpc_security_group_ids = [aws_security_group.My_Web_Server.id]
   user_data              = file ("user_data.sh")
