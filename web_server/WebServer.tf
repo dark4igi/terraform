7fba237b9d4278b69cac260475eeb076c885b63c
dark4igi <dakr4igi@gmail.com>
2020-08-23 23:50:10 +0300

    prevent_destroy = true

--- web_server/WebServer.tf
+++ web_server/WebServer.tf
@@ -10,12 +10,16 @@ resource "aws_instance" "My_Web_Server" {
   user_data              = templatefile("user_data.sh.tpl", {
     f_name = "igi",
     l_name = "moran",
-    names = ["Martos", "Vasya", "Kolya", "Petya", "John", "Donald", "Masha"]
+    names = ["Martos", "Vasya", "Kolya", "Petya", "John", "Donald", "Masha", "Test"]
   })
 
   tags = {
     Name = "Web Server"
     Owner = "igi"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
@@ -29,7 +33,7 @@ resource "aws_security_group" "My_Web_Server" {
   description = "Web server security group"
 
   dynamic "ingress" {
-    for_each = ["80", "443", "22", "8080"]
+    for_each = ["80", "443", "22", "8080", "9093"]
     content {
       from_port   = ingress.value
       to_port     = ingress.value
