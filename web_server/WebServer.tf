af2b71da548f106b32193834909d78fbd0e1d1fb
dark4igi <dakr4igi@gmail.com>
2020-08-24 00:18:54 +0300

    mv outputs

--- web_server/WebServer.tf
+++ web_server/WebServer.tf
@@ -60,19 +60,3 @@ resource "aws_security_group" "My_Web_Server" {
   }
 
 }
-
-output "web_server_instance_id" {
-  value = aws_instance.My_Web_Server.id
-}
-
-output "web_server_public_ip" {
-  value = aws_eip.mystatic_ip.public_ip
-}
-
-output "web_server_sg_id" {
-  value = aws_security_group.My_Web_Server.id
-}
-
-output "web_server_sg_arn" {
-  value = aws_security_group.My_Web_Server.arn
-}
