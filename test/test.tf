478ab5bfb9408c1e3d306bb8d8c6f926c28d8fe9
dark4igi <dakr4igi@gmail.com>
2020-08-24 13:20:01 +0300

    working vpc + subnets

---
+++ test/test.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "working" {}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "main" {
+  count = length ( data.aws_availability_zones.working.names )
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+  availability_zone = data.aws_availability_zones.working.names [ "${count.index}" ]
+
+  tags = {
+    Name = "Main subnet on ${data.aws_availability_zones.working.names [ "${count.index}" ]} az"
+  }
+}
+
+
+
+output "data_aws_availability_zone" {
+  value = data.aws_availability_zones.working.names[0]
+}
+
+output "data_aws_availability_zone_count" {
+  value = length ( data.aws_availability_zones.working.names )
+}
