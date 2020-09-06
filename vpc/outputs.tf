b1da2905e7337f9340f6fc55018c88b1ae3758be
ihor.yevtushenko <ihor.yevtushenko@refinitiv.com>
2020-09-07 00:00:06 +0300

    refactor

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,11 +1,11 @@
-output "data_aws_availability_zone" {
-  value = data.aws_availability_zones.working.names[0]
-}
-
 output "data_aws_availability_zone_count" {
   value = length ( data.aws_availability_zones.working.names )
 }
 
-#output "subnets" {
-#  value = aws_subnet.main.id
-#}
+output "data_aws_availability_zone_list" {
+  value = data.aws_availability_zones.working.names
+}
+
+output "subnets" {
+  value = [aws_subnet.main.*.id, aws_subnet.main.*.availability_zone]
+}
